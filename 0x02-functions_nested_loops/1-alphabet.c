999482a965b7beb1b790db3d6fd4fc193ffd4575
KING-EZE <omojuwamiracle@gmail.com>
2022-07-05 19:28:13 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - print in alphabet lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
