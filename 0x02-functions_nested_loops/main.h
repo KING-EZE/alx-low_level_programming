4834dc2ae28a9948c8d0d5529f3633e4ce08ca83
KING-EZE <omojuwamiracle@gmail.com>
2022-07-05 20:26:04 +0000

    mm

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,9 @@
 #include <unistd.h>
+#include "main.h"
 /**
+ * main -check the code for Holberton School students.
+ *
+ * Return: Always 0.
  * _putchar - writes the character c to stdout.
  * print_alphabet - prints alphabet and returns new line.
  * print_alphabet_x10 - print alphabet 10 times.
@@ -14,16 +18,20 @@
  * print_to_98 - prints all natural numbers to 98
  * print_times_table - print times table up to
  */
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+int main(void)
+{
+	int _putchar(char);
+	void print_alphabet(void);
+	void print_alphabet_x10(void);
+	int _islower(int c);
+	int _isalpha(int c);
+	int print_sign(int n);
+	int _abs(int);
+	int print_last_digit(int);
+	void jack_bauer(void);
+	void times_table(void);
+	int add(int, int);
+	void print_to_98(int n);
+	void print_times_table(int n);
+	return (0);
+}
