30744e0ae170fed7efc9ce1439defce0df97a496
KING-EZE <omojuwamiracle@gmail.com>
2022-07-06 16:58:50 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - determine sign of int
+ * @n: number to check for sign
+ * Return: 1 if positive, 0 if 0, and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('43');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('48');
+		return (0);
+	}
+	else if
+	{
+		_putchar('45');
+		return (0);
+	}
+	_putchar('\n');
+}
