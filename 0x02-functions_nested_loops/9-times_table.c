37404bd3cfa9cdbc8e400a344656f85e2b9b63e9
KING-EZE <omojuwamiracle@gmail.com>
2022-07-06 18:02:10 +0000

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table - print times table up to 9
+ * Return: void
+ */
+void times_table(void)
+{
+	int a, b, opp;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+		for (b = 0; b <= 9; b++)
+		{
+			opp = a * b;
+			_putchar(',');
+			_putchar(' ');
+			if (opp <= 9)
+			{
+				_putchar(' ');
+				_putchar((opp % 10) + '0');
+			}
+			else
+			{
+				_putchar((opp / 10) + '0');
+				_putchar((opp % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
