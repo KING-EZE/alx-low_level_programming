9b03683f0ee87d79a1a4a98d0ff437d73815b959
KING-EZE <omojuwamiracle@gmail.com>
2022-07-12 18:51:39 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - set value of int to 98
+ * @n: address of the value to change
+ * Description: reset *n to 98
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
