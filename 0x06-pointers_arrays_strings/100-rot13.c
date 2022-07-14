1bcf7ccc606fcbea46f92c560d5a775b2e2052f4
KING-EZE <omojuwamiracle@gmail.com>
2022-07-14 13:09:05 +0000

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input string
+ * Return: the pointer string
+ */
+
+char *rot13(char *s)
+{
+	int n = 0, i;
+
+	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + n) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + n) == letters[i])
+			{
+				*(s + n) == rot13[i];
+				break;
+			}
+		}
+		n++;
+	}
+	return (s);
+}
