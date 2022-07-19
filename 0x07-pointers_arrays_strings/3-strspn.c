22dfb6b46b89618ef885054b08c81e498025db40
KING-EZE <omojuwamiracle@gmail.com>
2022-07-19 22:36:00 +0000

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - get lenght of a prefix substring
+ * @s: string
+ * @accept: bytes being compared
+ * Description: finds number of bytes required to find accept in s
+ * Return: bytes required to find accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		k = 0;
+		for (j = 0; accept[j] != '\0'; J++)
+		{
+			if (accept[j] == s[i])
+			{
+				k = 1;
+			}
+		}
+		if (k == 0)
+		{
+			return (i);
+		}
+	}
+	return (i);
+}
