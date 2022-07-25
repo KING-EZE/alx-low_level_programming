26aea26db625010c2aea2ab7571a315e10467bab
KING-EZE <omojuwamiracle@gmail.com>
2022-07-25 12:18:19 +0000

    0-whatmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - fuction
+ * @argc: count of strings
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc_attribute_((unused)), char *argv[])
+{
+	printf("%s\n", argv[]);
+	return (0);
+}
