7979223cef4defe3097bb1b7858d60b097741595
KING-EZE <omojuwamiracle@gmail.com>
2022-08-04 22:53:21 +0000

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,12 +10,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array != NULL && cmp != NULL)
+	if (size > 0)
 	{
-		if (size <= 0)
-			return (-1);
-		for (; i < size; i++)
-			if (cmp(array[i]))
-				return (i);
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++;
+			}
+		}
 	}
+	return (-1);
 }
