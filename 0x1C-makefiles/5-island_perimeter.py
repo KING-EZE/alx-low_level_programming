6094f6b84f88fdfb1ccacdc1d5d75b9a497a1e6d
KING-EZE <omojuwamiracle@gmail.com>
2022-12-13 13:08:04 +0000

    fixed

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,7 +10,7 @@ def num_water_neighbors(grid, i, j):
     if i <= 0 or not grid[i - 1][j]:
         num += 1
     if j <= 0 or not grid[i][j - 1]:
-         num += 1
+        num += 1
     if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
         num += 1
     if i >= len(grid) - 1 or not grid[i + 1][j]:
